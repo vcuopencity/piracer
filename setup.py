6a99da2a9f7b3277dc11e8d00758b308d1521368
Adam Morrissett <morrissettal2@vcu.edu>
2021-01-28 14:18:51 -0500

    Update ROS2 entry point to robot
    setup.py
    * change ROS2 node entry point from platform to
      robot in resopnse to issue #5

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'platform = piracer.platform:main',
+            'robot = piracer.robot:main',
         ],
     },
 )
