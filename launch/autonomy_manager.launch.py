c63830a64c7ae16edc673a7698a82c5ef4ba41ec
Patrick Marshall <marshallpt@vcu.edu>
2021-07-08 13:56:19 -0400

    Added ackermann control to autonomy_manager launch
    Added the ability to control the car via ackermann messages
    when launching with the autonomy_manager launch file.
    
    autonomy_manager.launch.py
    * added all necessary nodes for ackermann control

--- launch/autonomy_manager.launch.py
+++ launch/autonomy_manager.launch.py
@@ -10,49 +10,58 @@ from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
-    bridge_topics = join(get_package_share_directory('piracer'),
-                         'config', 'command_bridge_topics.yaml')
-    # steering_config = join(get_package_share_directory('piracer'),
-    #                        'config', 'steering_config.yaml')
+    command_bridge_topics = join(get_package_share_directory('piracer'),
+                                 'config', 'command_bridge_topics.yaml')
+    ackermann_bridge_topics = join(get_package_share_directory('piracer'),
+                                   'config', 'ackermann_bridge_topics.yaml')
+    steering_config = join(get_package_share_directory('piracer'),
+                           'config', 'steering_config.yaml')
     return LaunchDescription([
         DeclareLaunchArgument('car_name', default_value='car1',
                               description='Sets the namespace for this car.'),
-        # Node(
-        #     package='piracer',
-        #     namespace=[LaunchConfiguration('car_name')],
-        #     executable='steering_driver',
-        #     name='steering_driver',
-        #     parameters=[steering_config]
-        # ),
-        # Node(
-        #     package='piracer',
-        #     namespace=[LaunchConfiguration('car_name')],
-        #     executable='throttle_driver',
-        #     name='throttle_driver'
-        # ),
-        # Node(
-        #     package='piracer',
-        #     namespace=[LaunchConfiguration('car_name')],
-        #     executable='display_driver',
-        #     name='display_driver'
-        # ),
-        # Node(
-        #     package='piracer',
-        #     namespace=[LaunchConfiguration('car_name')],
-        #     executable='power_monitor_driver',
-        #     name='power_monitor_driver'
-        # ),
-        # Node(
-        #     package='v4l2_camera',
-        #     namespace=[LaunchConfiguration('car_name')],
-        #     executable='v4l2_camera_node',
-        #     name='picamera_driver'
-        # ),
+        Node(
+            package='piracer',
+            namespace=[LaunchConfiguration('car_name')],
+            executable='steering_driver',
+            name='steering_driver',
+            parameters=[steering_config]
+        ),
+        Node(
+            package='piracer',
+            namespace=[LaunchConfiguration('car_name')],
+            executable='throttle_driver',
+            name='throttle_driver'
+        ),
+        Node(
+            package='piracer',
+            namespace=[LaunchConfiguration('car_name')],
+            executable='display_driver',
+            name='display_driver'
+        ),
+        Node(
+            package='piracer',
+            namespace=[LaunchConfiguration('car_name')],
+            executable='power_monitor_driver',
+            name='power_monitor_driver'
+        ),
+        Node(
+            package='v4l2_camera',
+            namespace=[LaunchConfiguration('car_name')],
+            executable='v4l2_camera_node',
+            name='picamera_driver'
+        ),
         Node(
             package='piracer',
             namespace=[LaunchConfiguration('car_name')],
             executable='autonomy_manager',
             name='autonomy_manager',
-            parameters=[bridge_topics]
+            parameters=[command_bridge_topics]
+        ),
+        Node(
+            package='piracer',
+            namespace=[LaunchConfiguration('car_name')],
+            executable='ackermann_controller',
+            name='ackermann_controller',
+            parameters=[ackermann_bridge_topics]
         )
     ])
