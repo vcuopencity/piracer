7e63d158f52db108f06e29090be3b55c075b3785
Patrick Marshall <marshallpt@vcu.edu>
2021-08-05 15:51:21 -0400

    Added basic subscribe functionality
    Added method to subscribe to other agents to receive their states.
    Callback is currentyly just a placeholder.
    
    car_config.yaml
    * added neighbor_list parameter
    
    car_state.py
    * aded _init_sub functionality

--- piracer/car_state.py
+++ piracer/car_state.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 
-from traffic_signal_msgs.msg import LanternState
+from traffic_signal_msgs.msg import SignalState
 from geometry_msgs.msg import PoseStamped
 
 
@@ -19,6 +19,8 @@ class CarState(Node):
     def __init__(self):
         super().__init__('car_state')
 
+        self._agent_name = self.get_namespace().lstrip('/')
+
         self._init_params()
         self._init_pub()
         self._init_sub()
@@ -33,6 +35,9 @@ class CarState(Node):
         self.declare_parameter('report_frequency', 1.0)
         self._report_freq = self.get_parameter('report_frequency').get_parameter_value().double_value
 
+        self.declare_parameter('neighbor_list', [])
+        self._neighbor_list = self.get_parameter('neighbor_list').get_parameter_value().string_array_value
+
     def _init_pub(self):
         self._car_state_pub = self.create_publisher(
             msg_type=PoseStamped,
@@ -41,17 +46,36 @@ class CarState(Node):
         )
 
     def _init_sub(self):
-        """No sensing data being collected / parsed as of yet."""
-        pass
+        """Subscribe to all neighbor state topics."""
+        for neighbor in self._neighbor_list:
+            if self._agent_name in neighbor:
+                return
+            elif "car" in neighbor:
+                self.create_subscription(
+                    msg_type=PoseStamped,
+                    topic='/' + neighbor + '/car_state',
+                    callback=self._neighbor_cb,
+                    qos_profile=10,
+                )
+            elif "signal" in neighbor:
+                self.create_subscription(
+                    msg_type=SignalState,
+                    topic='/' + neighbor + '/signal_state',
+                    callback=self._neighbor_cb,
+                    qos_profile=10,
+                )
 
     def _init_dict(self):
         self._agent_states = {}
         init_status = CarInfo(0, 0)
-        self._agent_states[self.get_namespace()] = init_status
+        self._agent_states[self._agent_name] = init_status
+
+    def _neighbor_cb(self, msg):
+        pass
 
     def _timer_cb(self):
         state_msg = PoseStamped()
-        this_car_status = self._agent_states[self.get_namespace()]
+        this_car_status = self._agent_states[self._agent_name]
 
         state_msg.pose.position.x = float(this_car_status.x)
         state_msg.pose.position.y = float(this_car_status.y)
