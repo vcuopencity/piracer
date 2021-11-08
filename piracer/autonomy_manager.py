6084e8a894619e37cc0a5e0b65f7cea8ef1ae383
Patrick Marshall <marshallpt@vcu.edu>
2021-11-08 15:16:08 -0500

    Refactored autonomy manager
    Refactored autonomy_manager to match the design of the other nodes,
    with individual _init_ methods for each portion of the init phase.
    
    autonomy_manager.py
    * refactored initialization of everything
    * made output messages more descriptive by including the agent name

--- piracer/autonomy_manager.py
+++ piracer/autonomy_manager.py
@@ -14,56 +14,67 @@ class AutonomyManager(Node):
     def __init__(self):
         super().__init__('autonomy_manager')
 
-        # Topic parameterd
+        self._mode_machine = None
+        self._agent_name = self.get_namespace().lstrip('/')
+
+        self._init_params()
+        self._init_pub()
+        self._init_sub()
+        self._init_srv()
+        self._init_state_machine()
+
+    def _init_params(self):
         self.declare_parameter('cmd_bridge_input_topic', 'control_input_topic')
-        input_topic = self.get_parameter('cmd_bridge_input_topic').get_parameter_value().string_value
+        self.cmd_bridge_input_topic = self.get_parameter('cmd_bridge_input_topic').get_parameter_value().string_value
 
         self.declare_parameter('cmd_bridge_output_topic', 'control_output_topic')
-        output_topic = self.get_parameter('cmd_bridge_output_topic').get_parameter_value().string_value
+        self.cmd_bridge_output_topic = self.get_parameter('cmd_bridge_output_topic').get_parameter_value().string_value
 
-        # Bridge subscriber / publisher
+    def _init_pub(self):
+        self.command_pub = self.create_publisher(
+            msg_type=Command,
+            topic=self.cmd_bridge_output_topic,
+            qos_profile=10,
+        )
+
+    def _init_sub(self):
         self.command_sub = self.create_subscription(
             msg_type=Command,
-            topic=input_topic,
+            topic=self.cmd_bridge_input_topic,
             callback=self.command_callback,
             qos_profile=10,
         )
-        self.command_pub = self.create_publisher(
-            msg_type=Command,
-            topic=output_topic,
-            qos_profile=10,
-        )
 
-        # State machine
-        states = ['auto', 'direct']
-        transitions = [
-            {'trigger': 'direct', 'source': '*', 'dest': 'direct'},
-            {'trigger': 'auto', 'source': '*', 'dest': 'auto'}
-        ]
-        machine = Machine(model=self, states=states, transitions=transitions, initial='direct')
-
-        # Service client
+    def _init_srv(self):
         self.enable_ackermann_client = self.create_client(Enable, 'ackermann_bridge_enable_service')
         while not self.enable_ackermann_client.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("ackermann_bridge_enable_service not available, waiting...")
         self.ack_request = Enable.Request()
 
+    def _init_state_machine(self):
+        states = ['auto', 'direct']
+        transitions = [
+            {'trigger': 'direct', 'source': '*', 'dest': 'direct'},
+            {'trigger': 'auto', 'source': '*', 'dest': 'auto'}
+        ]
+        self._mode_machine = Machine(model=self, states=states, transitions=transitions, initial='direct')
+
     # State Machine callbacks ---------------------------------------------------------------------
     # Direct mode
     def on_enter_direct(self):
-        self.get_logger().info("Car is now entering DIRECT mode.")
+        self.get_logger().info(f"{self._agent_name} is now entering DIRECT mode.")
         self.enable_ackermann()
 
     def on_exit_direct(self):
-        self.get_logger().info("Car is now exiting DIRECT mode.")
+        self.get_logger().info(f"{self._agent_name} is now exiting DIRECT mode.")
         self.disable_ackermann()
 
     # Auto mode
     def on_enter_auto(self):
-        self.get_logger().info("Car is now entering AUTO mode.")
+        self.get_logger().info(f"{self._agent_name} is now entering AUTO mode.")
 
     def on_exit_auto(self):
-        self.get_logger().info("Car is now exiting AUTO mode.")
+        self.get_logger().info(f"{self._agent_name} is now exiting AUTO mode.")
 
     # Service callbacks ---------------------------------------------------------------------------
     def enable_ackermann(self):
@@ -81,12 +92,12 @@ class AutonomyManager(Node):
 
         if msg.operational_mode.lower() == 'direct':
             if self.state == 'direct':
-                self.get_logger().info("Car is already in DIRECT mode!")
+                self.get_logger().info(f"{self._agent_name} is already in DIRECT mode!")
             else:
                 self.direct()
         if msg.operational_mode.lower() == 'auto':
             if self.state == 'auto':
-                self.get_logger().info("Car is already in AUTO mode!")
+                self.get_logger().info(f"{self._agent_name} is already in AUTO mode!")
             else:
                 self.auto()
 
