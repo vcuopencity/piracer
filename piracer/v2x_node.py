ea94ef3f942149ea84ea26aa3eff810bddf993e0
Patrick Marshall <marshallpt@vcu.edu>
2022-11-06 23:05:11 -0500

    Updated crash handling for V2X node.

--- piracer/v2x_node.py
+++ piracer/v2x_node.py
@@ -64,8 +64,9 @@ class V2xNode(Node):
             self.mqtt_client.connect(self._mqtt_broker_uri, 1883)
             self.mqtt_client.loop_start()
         except ConnectionRefusedError:
-            self.get_logger().error(f'ERROR: MQTT Publisher failed to connect to: {self._mqtt_broker_uri}')
-            self.get_logger().error(f'V2X is non-functional, as the MQTT client failed to connect to the broker.')
+            self.get_logger().fatal(f'MQTT Publisher failed to connect to: {self._mqtt_broker_uri}')
+            self.get_logger().fatal(f'V2X is non-functional, as the MQTT client failed to connect to the broker. Exiting...')
+            exit()
         
 
     def _mqtt_on_connect(self, client, userdata, flags, rc):
