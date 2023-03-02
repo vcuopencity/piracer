13a2348b199f1bf740bc2bef6671cfa6c965e3b8
Nathan Reyes <reyespinr@vcu.edu>
2023-03-02 12:08:48 -0500

    update imu and vive topic to be parameters parameters are passed through launch files individually

--- launch/localization.launch.py
+++ launch/localization.launch.py
@@ -1,5 +1,6 @@
 # Standard library imports
 from os.path import join
+from os import environ
 
 # Third-party imports
 from ament_index_python.packages import get_package_share_directory
@@ -16,10 +17,12 @@ def generate_launch_description():
     """ Launch hardware_nodes.launch and autonomy_manager node, as well as every control mode of the piracer using each
     respective launch file, passing launch_hardware=false so there aren't duplicates of the hardware nodes running.
     """
-    car_config = join(get_package_share_directory('piracer'),
-                      'config', 'car_config.yaml')
     ekf_config = join(get_package_share_directory('piracer'),
                       'config', 'ekf_config.yaml')
+
+    vive_topic = 'vive/' + environ['TRACKER_ID']
+    agent = 'car' + environ['CAR_ID']
+    imu_topic = agent + '/imu'
 
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
@@ -29,34 +32,33 @@ def generate_launch_description():
     with open(urdf, 'r') as infp:
         robot_desc = infp.read()
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            'agent_name',
-            default_value='car1',
-            description='Sets the namespace for this car.'),
-        Node(
-            package='bno055_driver',
-            namespace=[LaunchConfiguration('agent_name')],
-            executable='bno055_driver',
-            name='bno055_driver',
-            parameters=[car_config]
-        ),
-        Node(
-            package='robot_localization',
-            namespace=[LaunchConfiguration('agent_name')],
-            executable='ekf_node',
-            name='ekf_filter_node',
-            output='screen',
-            parameters=[ekf_config]
-        ),
-        Node(
-            package='robot_state_publisher',
-            namespace=[LaunchConfiguration('agent_name')],
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            # parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_desc}],
-            parameters=[{'robot_description': robot_desc}],
-            arguments=[urdf]
-        )
-    ])
+    ld = LaunchDescription()
+
+
+    robot_localization_node = Node(
+        package='robot_localization',
+        namespace=[LaunchConfiguration('agent_name')],
+        executable='ekf_node',
+        name='ekf_filter_node',
+        output='screen',
+        # parameters=[ekf_config],
+        parameters=[{
+            'odom0': vive_topic,
+            'imu0': imu_topic
+        }, ekf_config]
+    )
+
+    robot_publisher_node = Node(
+        package='robot_state_publisher',
+        namespace=[LaunchConfiguration('agent_name')],
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time, 'robot_description': robot_desc}],
+        arguments=[urdf]
+    )
+
+    ld.add_action(robot_localization_node, robot_publisher_node)
+
+    return ld
+
